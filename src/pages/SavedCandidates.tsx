d9d46c4ef16f2c671ce2d8f2a1d455f2a60ab1ef
Aditya Bhonsle <aditya.a.bhonsle@gmail.com>
2025-05-05 11:59:07 -0400

    removed filter and sort

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -6,8 +6,7 @@ import { IoRemoveCircle } from 'react-icons/io5';
 
 const SavedCandidates: React.FC = () => {
   const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
-  const [filter, setFilter] = useState('');
-  const [sortOption, setSortOption] = useState<'name' | 'location'>('name');
+
 
   useEffect(() => {
     const saved = JSON.parse(localStorage.getItem('savedCandidates') || '[]') as Candidate[];
@@ -20,27 +19,12 @@ const SavedCandidates: React.FC = () => {
     localStorage.setItem('savedCandidates', JSON.stringify(updatedCandidates));
   };
 
-  const filteredCandidates = savedCandidates.filter(candidate =>
-    (((candidate.name || candidate.login) || '').toLowerCase()).includes(filter.toLowerCase())
-  );
-
-  const sortedCandidates = filteredCandidates.sort((a, b) => {
-    if (sortOption === 'name') {
-      const nameA = (a.name || a.login) || '';
-      const nameB = (b.name || b.login) || '';
-      return nameA.localeCompare(nameB);
-    } else {
-      const locationA = a.location || '';
-      const locationB = b.location || '';
-      return locationA.localeCompare(locationB);
-    }
-  });
 
   return (
     <div className="saved-candidates-container">
       <h1>Potential Candidates</h1>
-      
-      {sortedCandidates.length > 0 ? (
+
+      {savedCandidates.length > 0 ? (
         <table className="saved-candidates-table">
           <thead>
             <tr>
@@ -54,13 +38,13 @@ const SavedCandidates: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {sortedCandidates.map(candidate => (
+            {savedCandidates.map(candidate => (
               <tr key={candidate.id}>
                 <td>
                   <img
-                  className="candidate-avatar-table"
-                  src={candidate.avatar_url}
-                  alt={candidate.login}
+                    className="candidate-avatar-table"
+                    src={candidate.avatar_url}
+                    alt={candidate.login}
                   />
                 </td>
                 <td>{candidate.name ? `${candidate.name} (${candidate.login})` : candidate.login}</td>
@@ -70,7 +54,7 @@ const SavedCandidates: React.FC = () => {
                 <td>{candidate.bio || 'N/A'}</td>
                 <td>
                   <IoRemoveCircle
-                  
+
                     onClick={() => removeCandidate(candidate.id || 0)}
                   />
                 </td>
